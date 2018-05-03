b6d6b560e336546a08c4a2c766adc2c664697baa
clifford dicarlo <cdc4444@gmail.com>
2018-05-03 13:23:37 -0400

    Initial Commit

---
+++ energyUsage/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  energyUsage
+//
+//  Created by Clifford Campo on 5/3/18.
+//  Copyright © 2018 CliffordCampo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
