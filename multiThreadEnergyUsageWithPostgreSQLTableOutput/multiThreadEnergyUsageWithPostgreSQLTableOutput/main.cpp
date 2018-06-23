30e669068845fd26be0ff263e0d59794c415561f
clifford dicarlo <cdc4444@gmail.com>
2018-06-23 14:02:46 -0400

    2018-06-23T13:57:31 Initial Entry

---
+++ multiThreadEnergyUsageWithPostgreSQLTableOutput/multiThreadEnergyUsageWithPostgreSQLTableOutput/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  multiThreadEnergyUsageWithPostgreSQLTableOutput
+//
+//  Created by Clifford Campo on 6/23/18.
+//  Copyright © 2018 CliffordCampo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+ 
+    std::cout << "Hello " << *(argv +0) << "!\n" << std::endl;
+       // insert code here...
+    return 0;
+}
